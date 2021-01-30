8eebe7888c50c7734e500ab0817b8aab46721ff9
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2021-01-30 20:20:43 +0000

    Fix unit tests

--- ca/server_test.go
+++ ca/server_test.go
@@ -1,27 +1,36 @@
 package ca
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-var TestPublicKey = []byte("ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHwXYROIrAfv9RS4LyCPdsPGy6EqM+vncrrZXzVJbNuV john@doe\n")
+var testPublicKey *PublicKey
+
+func init() {
+	var err error
+	testPublicKey, err = NewPublicKey("./testdata/test.pub")
+	if err != nil {
+		panic(fmt.Errorf("couldn't find test public key: %w", err))
+	}
+}
 
 func TestNewServer(t *testing.T) {
 	wd, err := os.Getwd()
 	s, err := NewServer("./testdata/test", "./testdata/test.pub", false)
 	assert.Nil(t, err, wd)
 	assert.Equal(t, s.PrivateKeyPath, "./testdata/test")
-	assert.Equal(t, s.PublicKey, TestPublicKey)
+	assert.Equal(t, s.PublicKey, testPublicKey)
 	assert.NotNil(t, s.sshKeygenLock)
 }
 
 func TestNewServerWithInferredPublicKey(t *testing.T) {
 	s, err := NewServer("./testdata/test", "", false)
 	assert.Nil(t, err)
-	assert.Equal(t, s.PublicKey, TestPublicKey)
+	assert.Equal(t, s.PublicKey, testPublicKey)
 }
 
 func TestNewServerWithMissingPrivateKey(t *testing.T) {
@@ -46,5 +55,5 @@ func TestServerGetCAPublicKey(t *testing.T) {
 	var reply PublicKeyReply
 	err = s.GetCAPublicKey(struct{}{}, &reply)
 	assert.Nil(t, err)
-	assert.Equal(t, reply.CAPublicKey, TestPublicKey)
+	assert.Equal(t, reply.CAPublicKey, testPublicKey)
 }
